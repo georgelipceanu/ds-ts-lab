9bbe852e70d10f2ff2038add9f93735d8ef6797a
georgelipceanu <george.lipceanu@gmail.com>
2025-09-18 18:41:33 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,45 @@
 import {Friend, Colleague } from './myTypes'
+import { friends, colleagues } from './01-basics'
+
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+function allOlder(friends: Friend[]) : string[] {
+    var s: string[] = []
+    for (const f of friends) {
+        f.age+=1
+        s.push(`${f.name} is now ${f.age}`)
+    }
+    return s
+}
+
+console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+
+console.log(highestExtension(colleagues.current));
+
+function addColleague(colleagues: Colleague[], name: string, department: string, email: string) {
+    const c: Colleague = {
+        name: name,
+        department: department,
+        contact: {
+            email: email,
+            extension: highestExtension(colleagues).contact.extension + 1
+        }
+    }
+    colleagues.push(c)
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
